5b631aca4be6861fb8ae163252b8ee75dc5d59c7
DionatanLeao <dionatan.ti@outlook.com>
2026-02-28 21:38:00 -0300

    feat: Creating the domain

---
+++ src/main/java/com/circuit_breaker/publication/domain/Publication.java
@@ -0,0 +1,17 @@
+package com.circuit_breaker.publication.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Publication {
+    private String title;
+    private String imageUrl;
+    private String text;
+    private List<Comment> comments;
+}
